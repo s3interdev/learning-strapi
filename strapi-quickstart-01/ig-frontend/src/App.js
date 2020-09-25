390db20565a0a0a8738299b64d079f25a11560c8
Michael Owuor <michael.owuor@gmail.com>
2020-09-25 21:57:23 +0300

    Using Props to pass data between components.

--- strapi-quickstart-01/ig-frontend/src/App.js
+++ strapi-quickstart-01/ig-frontend/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Posts from './components/pages/Posts';
 import './App.css';
 
+const post = {
+	likes: 5,
+	description: 'The Balloon Trip to Remember',
+	image: {
+		url: '/uploads/ballons_e8c3b6838b.jpg',
+	},
+};
+
 function App() {
 	return (
 		<div className='container'>
-			<Posts />
+			<Posts likes={post.likes} desc={post.description} url={post.image.url} />
 		</div>
 	);
 }
