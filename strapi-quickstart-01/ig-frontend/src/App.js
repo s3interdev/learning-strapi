0042a7cd08034e7dd2c1e3772da99eaa194af599
Michael Owuor <michael.owuor@gmail.com>
2020-09-25 20:17:28 +0300

    Introduction to React and components.

--- strapi-quickstart-01/ig-frontend/src/App.js
+++ strapi-quickstart-01/ig-frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Posts from './components/pages/Posts';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<div className='container'>
+				<h1>This is the BlogPics App</h1>
+				<p>2 + 2</p>
+				<p>{2 + 2}</p>
+				<Posts />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ strapi-quickstart-01/ig-frontend/src/components/pages/Posts.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Posts = () => <div className='post'>This is the Posts Page</div>;
+
+export default Posts;
