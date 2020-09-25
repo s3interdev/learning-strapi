a0ec8fa3d01beed02c197265516109e5c486831f
Michael Owuor <michael.owuor@gmail.com>
2020-09-25 22:12:53 +0300

    Rendering multiple components with Array.map

--- strapi-quickstart-01/ig-frontend/src/App.js
+++ strapi-quickstart-01/ig-frontend/src/App.js
@@ -2,18 +2,35 @@ import React from 'react';
 import Posts from './components/pages/Posts';
 import './App.css';
 
-const post = {
-	likes: 5,
-	description: 'The Balloon Trip to Remember',
-	image: {
-		url: '/uploads/ballons_e8c3b6838b.jpg',
+const posts = [
+	{
+		likes: 5,
+		description: 'The Balloon Trip to Remember 1',
+		image: {
+			url: '/uploads/ballons_e8c3b6838b.jpg',
+		},
 	},
-};
-
+	{
+		likes: 15,
+		description: 'The Balloon Trip to Remember 2',
+		image: {
+			url: '/uploads/ballons_e8c3b6838b.jpg',
+		},
+	},
+	{
+		likes: 3,
+		description: 'The Balloon Trip to Remember 3',
+		image: {
+			url: '/uploads/ballons_e8c3b6838b.jpg',
+		},
+	},
+];
 function App() {
 	return (
 		<div className='container'>
-			<Posts likes={post.likes} desc={post.description} url={post.image.url} />
+			{posts.map((post, index) => (
+				<Posts key={index} likes={post.likes} desc={post.description} url={post.image.url} />
+			))}
 		</div>
 	);
 }
