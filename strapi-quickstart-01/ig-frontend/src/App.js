3dde00eb8874caa1bd3e4035021b64401eea4a47
Michael Owuor <michael.owuor@gmail.com>
2020-09-28 20:25:02 +0300

    Installing React Router DOM.

--- strapi-quickstart-01/ig-frontend/src/App.js
+++ strapi-quickstart-01/ig-frontend/src/App.js
@@ -1,48 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Navbar from './components/layout/Navbar';
 import Posts from './components/pages/Posts';
 
 import './App.css';
-
-const mockPosts = [
-	{
-		likes: 5,
-		description: 'The Balloon Trip to Remember 1',
-		image: {
-			url: 'https://res.cloudinary.com/superior-software-solutions-limited/image/upload/v1601299100/ballons_b730ac2114.jpg',
-		},
-	},
-	{
-		likes: 15,
-		description: 'The Balloon Trip to Remember 2',
-		image: {
-			url: 'https://res.cloudinary.com/superior-software-solutions-limited/image/upload/v1601299100/ballons_b730ac2114.jpg',
-		},
-	},
-	{
-		likes: 3,
-		description: 'The Balloon Trip to Remember 3',
-		image: {
-			url: 'https://res.cloudinary.com/superior-software-solutions-limited/image/upload/v1601299100/ballons_b730ac2114.jpg',
-		},
-	},
-	{
-		likes: 3,
-		description: 'The Balloon Trip to Remember 4',
-		image: {
-			url: 'https://res.cloudinary.com/superior-software-solutions-limited/image/upload/v1601299100/ballons_b730ac2114.jpg',
-		},
-	},
-	{
-		likes: 3,
-		description: 'The Balloon Trip to Remember 5',
-		image: {
-			url: 'https://res.cloudinary.com/superior-software-solutions-limited/image/upload/v1601299100/ballons_b730ac2114.jpg',
-		},
-	},
-];
 
 const App = () => {
 	// default state set to an empty list
@@ -65,16 +28,18 @@ const App = () => {
 	}, []);
 
 	return (
-		<div>
-			<Navbar />
-			<div className='container'>
-				<div className='row'>
-					{posts.map((post, index) => (
-						<Posts key={index} likes={post.likes} desc={post.description} url={post.image.url} />
-					))}
+		<Router>
+			<div>
+				<Navbar />
+				<div className='container'>
+					<div className='row'>
+						{posts.map((post, index) => (
+							<Posts key={index} likes={post.likes} desc={post.description} url={post.image.url} />
+						))}
+					</div>
 				</div>
 			</div>
-		</div>
+		</Router>
 	);
 };
 
