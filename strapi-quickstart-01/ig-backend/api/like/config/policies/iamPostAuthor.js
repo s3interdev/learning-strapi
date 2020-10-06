34414de8465eb40f15aed074313472f6d0436b15
Michael Owuor <michael.owuor@gmail.com>
2020-10-06 23:12:49 +0300

    Creating policy to retrieve the likes that I get.

--- strapi-quickstart-01/ig-backend/api/like/config/policies/iamPostAuthor.js
+++ strapi-quickstart-01/ig-backend/api/like/config/policies/iamPostAuthor.js
@@ -1,5 +1,9 @@
 module.exports = async (ctx, next) => {
-  const targetUser = String(ctx.request.query.user);
+  if (!ctx.request.query["post.author"]) {
+    return ctx.unauthorized("Please specify a post.author{id}");
+  }
+
+  const targetUser = String(ctx.request.query["post.author"]);
   const loggedInUser = String(ctx.state.user.id);
 
   if (targetUser === loggedInUser) {
