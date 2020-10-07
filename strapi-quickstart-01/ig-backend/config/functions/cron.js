e6ec2f1986fdea82e786417114b349ab8b03d9d2
Michael Owuor <michael.owuor@gmail.com>
2020-10-07 03:31:39 +0300

    Setting up a cron job to send emails.

--- strapi-quickstart-01/ig-backend/config/functions/cron.js
+++ strapi-quickstart-01/ig-backend/config/functions/cron.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Cron config that gives you an opportunity
@@ -18,4 +18,28 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
+  // "0 0 0 * * *": async () => {
+  //   console.log("Once a day at midnight...");
+  //   // retrieve the users
+  //   const users = await ExtensionScriptApis.plugins[
+  //     "users-permissions"
+  //   ].services.user.fetchAll({});
+  //   const res = await Promise.all(
+  //     users.map(async (user) => {
+  //       // retrieve all posts by the user
+  //       const posts = await ExtensionScriptApis.services.post.find({
+  //         author: user.id,
+  //       });
+  //       console.log("Posts written today: ", posts.length);
+  //       const total = posts.reduce((acc, post) => acc + post.likes, 0);
+  //       // send email
+  //       await ExtensionScriptApis.plugins["email"].services.email.send({
+  //         to: user.email,
+  //         from: "strapi.admin@localhost.com",
+  //         subject: "Your Total Likes for Today",
+  //         text: `You got ${total} likes today...`,
+  //       });
+  //     })
+  //   );
+  // },
 };
--- strapi-quickstart-01/ig-backend/config/server.js
+++ strapi-quickstart-01/ig-backend/config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  cron: {
+    enabled: true,
+  },
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '8eb59c140da54c901362116c88d8b7f8'),
+      secret: env("ADMIN_JWT_SECRET", "8eb59c140da54c901362116c88d8b7f8"),
     },
   },
 });
